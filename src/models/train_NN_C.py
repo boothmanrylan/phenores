d99a693613d447f3db23d9417162d7e041016b0f
Rylan Boothman <boothmanrylan@gmail.com>
2018-02-06 12:41:22 -0700

    CHANGE: split train_neural_net into a file for classification and a file for regression

---
+++ src/models/train_NN_C.py
@@ -0,0 +1,28 @@
+import numpy as np
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv1D
+from keras.utils import to_categorical
+
+with open(snakemake.input[0], 'rb') as f:
+    x_train = pickle.load(f)
+with open(snakemake.input[1], 'rb') as f:
+    y_train = pickle.load(f)
+with open(snakemake.input[2], 'rb') as f:
+    x_test = pickle.load(f)
+with open(snakemake.input[3], 'rb') as f:
+    y_test = pickle.load(f)
+
+model = Sequential()
+model.add(Conv1D(filters=10,
+                 kernel_size=3,
+                 activation='relu',
+                 input_shape=x_train.shape[1:]))
+model.add(Flatten())
+model.add(Dense(y_train.shape[1], activation='softmax'))
+model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train)
+
+model.save(snakemake.output[0])
---
+++ src/models/train_NN_R.py
@@ -0,0 +1,28 @@
+import pickle
+import numpy as np
+from keras.wrappers.scikit_learn import KerasRegressor
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv1D
+from sklearn.externals import joblib
+
+
+with open(snakemake.input[0], 'rb') as f:
+    x_train = pickle.load(f)
+with open(snakemake.input[1], 'rb') as f:
+    y_train = pickle.load(f)
+
+model = Sequential()
+model.add(Conv1D(filters=10,
+                 kernel_size=3,
+                 activation='relu',
+                 input_shape=x_train.shape[1:]))
+model.add(Flatten())
+model.add(Dense(1, kernel_initializer='normal'))
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['accuracy'])
+y_train = np.asarray(y_train)
+
+model.fit(x_train, y_train)
+
+model.save(snakemake.output[0])
